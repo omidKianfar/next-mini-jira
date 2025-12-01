0a3ebf6f35e55049b6278199b6c7fefc2e74589b
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-01 14:40:36 +0330

    fix payment failed styles

--- src/components/pages/auth/signup/steps/payment-page/payment-fail.tsx
+++ src/components/pages/auth/signup/steps/payment-page/payment-fail.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Icon } from "@iconify/react";
 import { useRouter } from "next/navigation";
+import { Button } from "../../../imports";
 
 const PaymentFailedComponent = () => {
   const router = useRouter();
@@ -10,17 +12,39 @@ const PaymentFailedComponent = () => {
   };
 
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center gap-6">
-      <h1 className="text-2xl font-bold">Payment failed</h1>
+    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-[600px] h-[500px]  p-6 border-2 border-amber-300 rounded-lg bg-white">
+        <div className="flex justify-center mb-10 mt-8">
+          <Icon icon={"streamline-freehand:cash-payment-bill"} className="text-[150px] text-amber-500" />
+        </div>
 
-      <p>Your subscription is failed.</p>
+        <h1 className=" font-bold text-3xl text-red-600 ">Payment failed.</h1>
 
-      <button
-        onClick={finishHandler}
-        className="px-6 py-3 bg-blue-600 text-white rounded-md"
-      >
-        Finish
-      </button>
+        <p className=" mt-4 mb-6">
+          Please check your card details or try another payment method. If the
+          amount has been deducted from your account but the payment still shows
+          as failed, please contact support for further assistance.
+        </p>
+
+        <div className="flex justify-center">
+          <Button
+            onClick={finishHandler}
+            icon={
+              <Icon
+                icon="grommet-icons:link-next"
+                width={16}
+                className="ml-2 mt-1"
+              />
+            }
+            className="mt-6 bg-blue-500 text-white border-2
+                 hover:bg-transparent hover:border-blue-500
+               hover:text-blue-500 rounded-lg px-8 py-2 
+                transition-all duration-200"
+          >
+            Go To Payment
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
