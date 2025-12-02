f3498c4f67cf8e80471fb90f058aac154d2153ff
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-02 23:00:17 +0330

    fix framer animation

--- src/components/atom/animation/index.tsx
+++ src/components/atom/animation/index.tsx
@@ -2,13 +2,16 @@
 
 import { motion } from "framer-motion";
 import { FarmerMotionProps } from "./type";
+import { useIsMobile } from "@/src/hooks/mobile-size";
 
 const FramerMotion = ({ children }: FarmerMotionProps) => {
+  const isMobile = useIsMobile();
+
   return (
     <motion.div
-      initial={{ opacity: 0, x: 50 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -10 }}
+      initial={isMobile ? { opacity: 0, y: 30 } : { opacity: 0, x: 50 }}
+      animate={isMobile ? { opacity: 1, y: 0 } : { opacity: 1, x: 0 }}
+      exit={isMobile ? { opacity: 0, y: -10 } : { opacity: 0, x: -10 }}
       transition={{ duration: 1, ease: "easeOut" }}
       className="w-full h-full flex items-center justify-center "
     >
