b3aae8061447319c87c51f624df9da998025fc7b
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-04 14:22:14 +0330

    fix lib folder types

---
+++ src/lib/tasks/type.ts
@@ -0,0 +1,15 @@
+import { AppDispatch } from "@/src/store";
+
+export interface ListenToTasksProps {
+  userId: string;
+  dispatch: AppDispatch;
+}
+
+export interface UpdateTaskStatusProps {
+  id: string;
+  status: string;
+}
+
+export interface FetchTaskProps {
+  taskId: string;
+}
