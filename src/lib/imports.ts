128a69762bf287e939d141aa232d30448113757a
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-04 14:21:59 +0330

    fix lib folder imports

--- src/lib/imports.ts
+++ src/lib/imports.ts
@@ -1,7 +1,10 @@
-export { db } from "@/config";
-export type { MyUserType, Task } from "@/src/types/global";
-export { UserType } from "@/src/types/global";
-export type { User } from "firebase/auth";
-export { doc, getDoc, setDoc } from "firebase/firestore";
 export { default as dayjs } from "dayjs";
 
+export { db } from "@/config";
+export { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+export { collection, onSnapshot, query, where } from "firebase/firestore";
+
+export type { MyUserType, Task } from "@/src/types/global";
+export type { User } from "firebase/auth";
+
+export { UserType } from "@/src/types/global";
