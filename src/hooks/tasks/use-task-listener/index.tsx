0412e6ca1421690222efe393b1df49591c5eb21b
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-04 14:21:38 +0330

    feat use task listener hook

---
+++ src/hooks/tasks/use-task-listener/index.tsx
@@ -0,0 +1,14 @@
+import { listenToTasks, useDispatch, useEffect } from "../../imports";
+import { useTaskListenerProps } from "../../type";
+
+export const useTaskListener = ({ user }: useTaskListenerProps) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!user) return;
+
+    const unsub = listenToTasks({ userId: user.userId, dispatch });
+
+    return () => unsub();
+  }, [user]);
+};
