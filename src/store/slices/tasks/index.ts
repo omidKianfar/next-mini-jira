5b591c189b6b113ce268a06b03915b53c8214ba5
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-04 14:14:18 +0330

    remove edit task from RTK

--- src/store/slices/tasks/index.ts
+++ src/store/slices/tasks/index.ts
@@ -15,19 +15,12 @@ const taskSlice = createSlice({
     addTask: (state, action: PayloadAction<Task>) => {
       state.tasks.push(action.payload);
     },
-    updateTask: (state, action: PayloadAction<Task>) => {
-      const index = state.tasks.findIndex(
-        (task) => (task.id = action.payload.id)
-      );
-
-      if (index !== -1) state.tasks[index] = action.payload;
-    },
     removeTask: (state, action: PayloadAction<string>) => {
       state.tasks = state.tasks.filter((task) => task.id !== action.payload);
     },
   },
 });
 
-export const { setTasks, addTask, updateTask, removeTask } = taskSlice.actions;
+export const { setTasks, addTask, removeTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
