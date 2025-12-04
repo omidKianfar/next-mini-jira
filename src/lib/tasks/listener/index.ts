e462d4551b4deb60ae8922d90a3ee96964eb6292
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-04 14:22:54 +0330

    feat task listener

---
+++ src/lib/tasks/listener/index.ts
@@ -0,0 +1,17 @@
+import { ListenToTasksProps } from "../type";
+
+import { collection, db, onSnapshot, query, Task, where } from "../../imports";
+import { setTasks } from "@/src/store/slices/tasks";
+
+export const listenToTasks = ({ userId, dispatch }: ListenToTasksProps) => {
+  const userIdInTasksQuery = query(
+    collection(db, "tasks"),
+    where("userId", "==", userId)
+  );
+
+  return onSnapshot(userIdInTasksQuery, (snapshot) => {
+    const tasks = snapshot.docs.map((doc) => doc.data() as Task);
+
+    dispatch(setTasks(tasks));
+  });
+};
