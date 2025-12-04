518539225d5e771d201d1af8537c9913a5dd3dbe
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-04 19:50:22 +0330

    fix task cart styles

--- src/components/pages/dahboard/board/task/index.tsx
+++ src/components/pages/dahboard/board/task/index.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import { useDraggable, useRouter } from "../../imports";
 import { TaskCardProps } from "../type";
+import { stringSlicer } from "@/src/components/atom/string-slicer";
 
 export const TaskCardComponent = ({ id, task }: TaskCardProps) => {
   const router = useRouter();
@@ -14,32 +15,41 @@ export const TaskCardComponent = ({ id, task }: TaskCardProps) => {
     <div
       ref={setNodeRef}
       style={style}
-      className="p-3 bg-white border-2 border-blue-300 rounded-lg mb-2  flex justify-between items-center"
+      className=" p-3 bg-white border-2 border-blue-300 rounded-lg mb-2  flex justify-between items-center"
     >
       <div
-        className="w-[95%] flex justify-start items-center cursor-grab "
+        className="w-[93%] flex justify-start items-center cursor-grab pr-1"
         {...listeners}
         {...attributes}
       >
-        <Icon
-          icon={
-            task.tag == "bug"
-              ? "solar:bug-bold-duotone"
-              : "material-symbols:task"
-          }
-          className="mr-1 text-amber-500 text-2xl"
-        />
+        <div>
+          <Icon
+            icon={
+              task.tag == "bug"
+                ? "solar:bug-bold-duotone"
+                : "material-symbols:task"
+            }
+            className="mr-1 text-amber-500 text-2xl"
+          />
+        </div>
 
-        <p>{task.title}</p>
+        <div>
+          <p className="break-all">{stringSlicer({ string: task.title, slice: 50 })}</p>
+          <p className="break-all">
+            {stringSlicer({ string: task.description, slice: 100 })}
+          </p>
+        </div>
       </div>
 
-      <div className="w-[5%] ">
+      <div className="w-[7%] ">
         <Icon
           data-no-dnd="true"
           icon="grommet-icons:link-next"
-          width={20}
-          className="hover:text-red-500 cursor-pointer text-amber-500"
-          onClick={() => router.push(`/dashboard/task-detail?taskId=${task.id}`)}
+          width={24}
+          className=" cursor-pointer text-amber-500 hover:text-amber-700 "
+          onClick={() =>
+            router.push(`/dashboard/task-detail?taskId=${task.id}`)
+          }
         />
       </div>
     </div>
