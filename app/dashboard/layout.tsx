b5af50f4b0eebc6d98b33f089ad115673b78a78b
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-06 07:56:43 +0330

    fix pages styles

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,14 +3,16 @@
 import { PropsWithChildren } from "react";
 import AuthGuard from "@/src/guards/auth-guard";
 import ProvidersWrapper from "@/src/providers";
-
+import DashboardHeader from "@/src/components/pages/dahboard/header";
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
- 
   return (
-    <div className="w-screen h-screen overflow-hidden bg-gray-100">
+    <div className="w-screen h-screen  bg-gray-100 overflow-y-auto scrollbar-hide">
       <ProvidersWrapper>
-        <AuthGuard>{children}</AuthGuard>
+        <AuthGuard>
+          <DashboardHeader />
+          {children}
+        </AuthGuard>
       </ProvidersWrapper>
     </div>
   );
