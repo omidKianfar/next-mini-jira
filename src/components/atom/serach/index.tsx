884a32a67583195a8efd7b2c09592399c4d5533d
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-06 09:20:51 +0330

    fix search tasks

--- src/components/atom/serach/index.tsx
+++ src/components/atom/serach/index.tsx
@@ -8,8 +8,9 @@ import { useSelector } from "react-redux";
 import { stringSlicer } from "../string-slicer";
 import { useIsMobile } from "@/src/hooks/mobile-size";
 import { Icon } from "@iconify/react";
+import { ModalProps } from "../modal/type";
 
-const SearchTasks = () => {
+const SearchTasks = ({ handleClose }: Pick<ModalProps, "handleClose">) => {
   const router = useRouter();
 
   const tasks = useSelector((state: RootState) => state.tasks.tasks);
@@ -51,15 +52,17 @@ const SearchTasks = () => {
           className="absolute top-3 left-1 text-2xl text-blue-500"
         />
 
-        {filteredTasks.length > 0 && <Icon
-          icon={"material-symbols:close-rounded"}
-          className="absolute top-3 right-1 text-2xl text-gray-400 hover:text-red-500 cursor-pointer"
-          onClick={() => {
-            setSearchValue("");
-            setFilteredTasks([]);
-            setOpenMenu(false);
-          }}
-        />}
+        {filteredTasks.length > 0 && (
+          <Icon
+            icon={"material-symbols:close-rounded"}
+            className="absolute top-3 right-1 text-2xl text-gray-400 hover:text-red-500 cursor-pointer"
+            onClick={() => {
+              setSearchValue("");
+              setFilteredTasks([]);
+              setOpenMenu(false);
+            }}
+          />
+        )}
 
         <input
           className="w-full my-1 border-2 py-2 rounded-lg border-blue-400 focus:outline-blue-600  text-sm px-8 shadow"
@@ -75,9 +78,10 @@ const SearchTasks = () => {
               <div
                 key={task.id}
                 className=" bg-white border-2 border-amber-400 rounded-lg mb-2  flex justify-between items-center cursor-pointer "
-                onClick={() =>
-                  router.push(`/dashboard/task-detail?taskId=${task.id}`)
-                }
+                onClick={() => {
+                  router.push(`/dashboard/task-detail?taskId=${task.id}`);
+                  handleClose();
+                }}
               >
                 <div className="w-full">
                   <div className="flex justify-between items-center p-1 bg-amber-400 rounded-t-lg ">
