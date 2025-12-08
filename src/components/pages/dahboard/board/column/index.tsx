801178cc68c26dc03864656de6c22eaf683ccc49
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-08 10:40:01 +0330

    fix column styles

--- src/components/pages/dahboard/board/column/index.tsx
+++ src/components/pages/dahboard/board/column/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ColumnProps } from "../../../type";
-import { Icon, useDroppable } from "../../../imports";
+import { MyIcon, useDroppable } from "../../../imports";
 
 const ColumnComponent = ({ id, children }: ColumnProps) => {
   const { setNodeRef, isOver } = useDroppable({ id });
@@ -9,7 +9,7 @@ const ColumnComponent = ({ id, children }: ColumnProps) => {
   return (
     <div
       ref={setNodeRef}
-      className={`w-full lg:w-80 bg-gray-100  rounded-lg  transition border-2 border-blue-300 shadow-2xl
+      className={`w-full lg:w-80  rounded-lg  transition border-2 border-blue-300 shadow-2xl
       ${isOver ? "bg-blue-300" : "bg-white"}
       `}
       style={{
@@ -19,18 +19,17 @@ const ColumnComponent = ({ id, children }: ColumnProps) => {
     >
       <h1
         className="mb-2 text-center capitalize font-bold  text-white
-bg-gradient-to-r from-blue-500 via-blue-400 to-blue-500
-
+          bg-gradient-to-r from-blue-500 via-blue-400 to-blue-500
           rounded-t-lg p-2 shadow flex justify-center items-center gap-2
           "
       >
         {id}
         {id == "todo" ? (
-          <Icon icon={"ri:todo-line"} className="text-2xl" />
+          <MyIcon icon={"ri:todo-line"} className="text-2xl" />
         ) : id == "inprogress" ? (
-          <Icon icon={"carbon:in-progress"} className="text-2xl" />
+          <MyIcon icon={"carbon:in-progress"} className="text-2xl" />
         ) : (
-          <Icon icon={"ant-design:file-done-outlined"} className="text-2xl" />
+          <MyIcon icon={"ant-design:file-done-outlined"} className="text-2xl" />
         )}
       </h1>
 
