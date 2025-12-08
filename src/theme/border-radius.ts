c82e39bb101cc07ad8fac342f236cdcd8b1dc09f
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-08 10:32:23 +0330

    fix tailwind configs and imports themes to it

---
+++ src/theme/border-radius.ts
@@ -0,0 +1,8 @@
+export const BorderRadius = {
+  sm: "4px",
+  DEFAULT: "8px",
+  md: "10px",
+  lg: "12px",
+  xl: "16px",
+  full: "9999px",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+import { Colors } from "./src/theme/colors";
+import { FontSize } from "./src/theme/fon-size";
+import { BorderRadius } from "./src/theme/border-radius";
+import { BoxShadow } from "./src/theme/box-shadow";
+
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors: {
-        brand: "#000",
+      colors: Colors,
+      fontFamily: {
+        sans: ["Poppins", "Inter", "Outfit", "system-ui", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
       },
+      fonSize: FontSize,
+      borderRadius: BorderRadius,
+      boxShadow: BoxShadow,
     },
   },
   plugins: [],
