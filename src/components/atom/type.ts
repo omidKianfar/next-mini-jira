e72c69d74cbcef656e721fca7cae5408c1ed1a69
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-08 15:57:20 +0330

    fix types

--- src/components/atom/type.ts
+++ src/components/atom/type.ts
@@ -96,3 +96,12 @@ export interface MyIconProps {
   icon: string;
   iconClass?: string;
 }
+
+export interface MyImageProps {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+  className?: string;
+  wrapperClass?: string;
+}
