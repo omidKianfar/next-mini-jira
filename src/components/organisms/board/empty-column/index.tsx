b5183012427366a932c9ca9d83ae11cc87dbe543
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-11 15:34:46 +0330

    feat emty column

---
+++ src/components/organisms/board/empty-column/index.tsx
@@ -0,0 +1,9 @@
+const EmptyColumn = () => {
+  return (
+    <div className="rounded-lg border-2 border-gray-400 p-2 text-center text-body shadow-md">
+      <p className="text-gray-500">No Data</p>
+    </div>
+  );
+};
+
+export default EmptyColumn;
