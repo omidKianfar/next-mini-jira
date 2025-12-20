405ed0815d04fe7a384251a9b04c080bec546f0a
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-20 14:33:45 +0330

    improve use auth listener

--- src/hooks/auth/use-user-listener.ts
+++ src/hooks/auth/use-user-listener.ts
@@ -1,25 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, onSnapshot } from "firebase/firestore";
 
-// type
 import { UseAuthListenerProps } from "../type";
-
-// config
 import { auth, db } from "@/config/firebase";
-
-// firestore
 import { findFirestoreUser } from "@/src/lib/auth/user-finder";
+import { MyUserType } from "@/src/types/global";
 
 export const useUserListener = ({
-  state,
   dispatch,
   unsubDocRef,
 }: UseAuthListenerProps) => {
+  const lastUserRef = useRef<MyUserType | null>(null);
+
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+    const unsubscribeAuth = onAuthStateChanged(auth, async (currentUser) => {
       if (!currentUser) {
         dispatch({
           type: "INITIALIZE",
@@ -33,39 +30,51 @@ export const useUserListener = ({
 
         if (unsubDocRef.current) {
           unsubDocRef.current();
-
           unsubDocRef.current = null;
         }
 
+        lastUserRef.current = null;
         return;
+      }
+
+      if (unsubDocRef.current) {
+        unsubDocRef.current();
+        unsubDocRef.current = null;
       }
 
       const ref = doc(db, "users", currentUser.uid);
 
-      if (unsubDocRef.current) unsubDocRef.current();
-
       unsubDocRef.current = onSnapshot(ref, async (snap) => {
-        const data = snap.exists() ? snap.data() : {};
+        const newUserData = snap.exists() ? snap.data() : {};
 
-        const newUser = await findFirestoreUser(currentUser);
+        const newFullUser = await findFirestoreUser(currentUser);
 
-        if (JSON.stringify(state.user) !== JSON.stringify(newUser)) {
-          dispatch({
-            type: "INITIALIZE",
-            payload: {
-              user: newUser,
-              isAuthenticated: true,
-              isLoading: null,
-              isInitialized: true,
-            },
-          });
+        if (
+          JSON.stringify(lastUserRef.current) === JSON.stringify(newFullUser)
+        ) {
+          return;
         }
+
+        lastUserRef.current = newFullUser;
+
+        dispatch({
+          type: "INITIALIZE",
+          payload: {
+            user: newFullUser,
+            isAuthenticated: true,
+            isLoading: null,
+            isInitialized: true,
+          },
+        });
       });
     });
 
     return () => {
-      unsubscribe();
-      if (unsubDocRef.current) unsubDocRef.current();
+      unsubscribeAuth();
+      if (unsubDocRef.current) {
+        unsubDocRef.current();
+        unsubDocRef.current = null;
+      }
     };
-  }, [state.user]);
+  }, []);
 };
