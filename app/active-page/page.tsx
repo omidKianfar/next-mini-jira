aba9463e44cfea1518a668d746d265b6e77ab98a
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-20 11:20:50 +0330

    feat active page

---
+++ app/active-page/page.tsx
@@ -0,0 +1,12 @@
+import FramerMotion from "@/src/components/atom/animation";
+import ActivePageComponent from "@/src/components/pages/active-page";
+
+const ActivePage = () => {
+  return (
+    <FramerMotion>
+      <ActivePageComponent />
+    </FramerMotion>
+  );
+};
+
+export default ActivePage;
