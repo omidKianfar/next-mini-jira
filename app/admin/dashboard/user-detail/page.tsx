66e79d8dd6fafdbeda04c98d32927332114e28cb
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-21 11:38:06 +0330

    feat dashboard user page

---
+++ app/admin/dashboard/user-detail/page.tsx
@@ -0,0 +1,12 @@
+import FramerMotion from "@/src/components/atom/animation";
+import AdminUserDetailComponent from "@/src/components/pages/admin/dashboard/user-detail";
+
+const AdminUserDetailPage = () => {
+  return (
+    <FramerMotion>
+      <AdminUserDetailComponent />
+    </FramerMotion>
+  );
+};
+
+export default AdminUserDetailPage;
---
+++ src/components/pages/admin/dashboard/user-detail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminUserDetailComponent = () => {
+  return <div>AdminUserDetailComponent</div>;
+};
+
+export default AdminUserDetailComponent;
