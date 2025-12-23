f700e183beda48b5206ecc832f3b8d0b4ec0c373
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-23 09:20:07 +0330

    fix pagination component

--- src/components/atom/pagination/index.tsx
+++ src/components/atom/pagination/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// type
 import { PaginationProps } from "../type";
 
 const PaginationComponent = ({
@@ -40,12 +41,12 @@ const PaginationComponent = ({
   };
 
   return (
-    <div className="mt-6 flex items-center justify-center gap-4">
-      <div className="flex items-center justify-center gap-2">
+    <div className="mt-4 flex items-center justify-center gap-2">
+      <div className="flex justify-center gap-2 rounded-md border border-dashed border-gray-400 bg-white p-2 shadow-md lg:flex-row">
         <button
           onClick={() => onPageChange(1)}
           disabled={currentPage === 1}
-          className="rounded bg-gray-200 px-3 py-1 disabled:opacity-50"
+          className="rounded border-2 border-gray-300 bg-gray-300 px-3 py-1 disabled:opacity-50"
         >
           {"<<"}
         </button>
@@ -53,7 +54,7 @@ const PaginationComponent = ({
         <button
           onClick={() => onPageChange(currentPage - 1)}
           disabled={currentPage === 1}
-          className="rounded bg-gray-200 px-3 py-1 disabled:opacity-50"
+          className="rounded border-2 border-gray-300 bg-gray-300 px-3 py-1 disabled:opacity-50"
         >
           {"<"}
         </button>
@@ -67,7 +68,7 @@ const PaginationComponent = ({
             <button
               key={p}
               onClick={() => onPageChange(p as number)}
-              className={`rounded px-3 py-1 ${
+              className={`rounded border-2 border-gray-300 px-3 py-1 ${
                 p === currentPage
                   ? "bg-primary-500 text-white"
                   : "bg-gray-200 hover:bg-gray-300"
@@ -81,7 +82,7 @@ const PaginationComponent = ({
         <button
           onClick={() => onPageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
-          className="rounded bg-gray-200 px-3 py-1 disabled:opacity-50"
+          className="rounded border-2 border-gray-300 bg-gray-300 px-3 py-1 disabled:opacity-50"
         >
           {">"}
         </button>
@@ -89,23 +90,25 @@ const PaginationComponent = ({
         <button
           onClick={() => onPageChange(totalPages)}
           disabled={currentPage === totalPages}
-          className="rounded bg-gray-200 px-3 py-1 disabled:opacity-50"
+          className="rounded border-2 border-gray-300 bg-gray-300 px-3 py-1 disabled:opacity-50"
         >
           {">>"}
         </button>
       </div>
 
-      <select
-        value={pageSize}
-        onChange={(e) => onPageSizeChange(Number(e.target.value))}
-        className="cursor-pointer rounded border px-2 py-1"
-      >
-        <option value={5}>5</option>
-        <option value={10}>10</option>
-        <option value={15}>15</option>
-        <option value={20}>20</option>
-        <option value={50}>50</option>
-      </select>
+      <div className="rounded-md border border-dashed border-gray-400 bg-white p-2 shadow-md">
+        <select
+          value={pageSize}
+          onChange={(e) => onPageSizeChange(Number(e.target.value))}
+          className="cursor-pointer rounded border border-gray-300 px-2 py-1"
+        >
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={15}>15</option>
+          <option value={20}>20</option>
+          <option value={50}>50</option>
+        </select>
+      </div>
     </div>
   );
 };
