50d62a17e779b92cb2928d42d61ca25203aa1069
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-23 09:19:41 +0330

    fix list component

---
+++ src/components/atom/lits/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+// type
+import { ListComponentProps } from "../type";
+
+const ListComponent = ({ children }: ListComponentProps) => {
+  return (
+    <div className="h-full w-full space-y-3 rounded-lg bg-white p-2 shadow">
+      <div className="scrollbar-show flex max-h-[calc(100vh-180px)] flex-col gap-3 overflow-y-auto">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ListComponent;
