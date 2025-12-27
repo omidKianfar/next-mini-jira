4ca9f7a311042fa33f63fa18929ecb2c7090473f
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-27 14:52:53 +0330

    fix navigation

--- src/hooks/navigation/index.tsx
+++ src/hooks/navigation/index.tsx
@@ -3,9 +3,10 @@
 import { useRouter } from "next/navigation";
 
 // routes
-import { routes } from "@/src/lib/route/routes";
+import { routes } from "@/src/helper/routes/routes";
 
 export const useNavigation = () => {
+  // hooks
   const router = useRouter();
 
   return {
@@ -19,10 +20,14 @@ export const useNavigation = () => {
     profile: () => router.push(routes.dashboard.profile),
     changePassword: () => router.push(routes.dashboard.changePassword),
     payment: () => router.push(routes.dashboard.payment),
+    support: () => router.push(routes.dashboard.support),
     taskDetail: (taskId: string) =>
       router.push(routes.dashboard.taskDetail(taskId)),
     adminDashboard: () => router.push(routes.admin.dashboard),
     adminProfile: () => router.push(routes.admin.profile),
+    adminSupport: () => router.push(routes.admin.support),
+    adminSupportChat: (chatId: string) =>
+      router.push(routes.admin.supportChat(chatId)),
     adminUserDetail: (userId: string) =>
       router.push(routes.admin.userDetail(userId)),
   };
