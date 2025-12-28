22cc0bbdf181fcb6ff59b67b299f47bf67370ec5
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-28 15:05:25 +0330

    fix tables styles

--- src/components/atom/table/index.tsx
+++ src/components/atom/table/index.tsx
@@ -3,15 +3,15 @@ import { TableProps } from "../type";
 
 export function TableComponent<T>({ data, columns }: TableProps<T>) {
   return (
-    <div className="w-full overflow-hidden rounded-lg bg-white shadow-lg">
+    <div className="w-full overflow-hidden rounded-lg border-2 border-warning-500 bg-white shadow-lg">
       <table className="w-full">
-        <thead className="border border-dashed border-gray-400 bg-gray-300 shadow-md">
+        <thead className="bg-gray-200 shadow-md border-b border-gray-300">
           <tr>
             {columns.map((col, idx) => {
               const head = col.head;
 
               return (
-                <th key={idx} className="p-4 text-left ">
+                <th key={idx} className="p-4 text-left">
                   {typeof head === "function" ? head({} as T) : head}
                 </th>
               );
@@ -21,7 +21,10 @@ export function TableComponent<T>({ data, columns }: TableProps<T>) {
 
         <tbody>
           {data.map((row: T, rowIndex) => (
-            <tr key={rowIndex} className="border border-dashed border-gray-400 hover:bg-gray-100">
+            <tr
+              key={rowIndex}
+              className="border-t border-dashed border-gray-300 hover:bg-gray-100"
+            >
               {columns.map((col, colIndex) => {
                 const cell = col.column;
 
