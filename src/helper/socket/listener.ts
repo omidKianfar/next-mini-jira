311777d99763b82887faf5a650334d9a70666b67
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-28 15:14:46 +0330

    initialize socket io

---
+++ src/helper/socket/listener.ts
@@ -0,0 +1,27 @@
+import { io } from "socket.io-client";
+
+// config
+import { SOCKET_URL } from "@/config/socket";
+
+export const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+  reconnection: true,
+});
+
+export const subscribeEvent = (
+  eventName: string,
+  callback: (...args: any[]) => void,
+) => {
+  socket.on(eventName, callback);
+};
+
+export const unsubscribeEvent = (
+  eventName: string,
+  callback?: (...args: any[]) => void,
+) => {
+  if (callback) {
+    socket.off(eventName, callback);
+  } else {
+    socket.off(eventName);
+  }
+};
