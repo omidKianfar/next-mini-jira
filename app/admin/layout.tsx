859eed77006ada431a6126b4eb2a1911b1a75b75
omid kianfar <kianfar.omid.990@gmail.com>
2025-12-28 15:02:18 +0330

    fix admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,14 +4,17 @@ import { PropsWithChildren, useState } from "react";
 
 // guard
 import AuthGuard from "@/src/guards/auth-guard";
+import RoleGuard from "@/src/guards/role-guard";
 
 // ui
 import Header from "@/src/components/organisms/header/indx";
-import SideBar from "@/src/components/organisms/sidebar";
-import RoleGuard from "@/src/guards/role-guard";
+import SideBar from "@/src/components/organisms/sidebar/user-sidebar/index";
+
+// type
 import { UserType } from "@/src/types/global";
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
+  // states
   const [showSidebar, setShowSidebar] = useState(false);
 
   return (
