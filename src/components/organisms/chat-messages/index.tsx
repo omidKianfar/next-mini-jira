f7a8cd53e5ee43e2c3cdc2e7bdad85fd8152751a
omid kianfar <kianfar.omid.990@gmail.com>
2026-02-22 18:18:55 +0330

    fix button and auto scroll for chat sidebar and chatbox

--- src/components/organisms/chat-messages/index.tsx
+++ src/components/organisms/chat-messages/index.tsx
@@ -1,10 +1,13 @@
 import { usePathname } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
 
 // type
 import { ChatMessagesProps } from '../type';
 
 // ui
 import ChatMessageItem from '../../molecule/chat/chat-message-item';
+import ButtonFreeClass from '../../atom/buttons-component/button-free-class';
+import MyIcon from '../../atom/icon-components';
 
 const ChatMessages = ({
   showMenu,
@@ -12,9 +15,40 @@ const ChatMessages = ({
   handleTemplateSelect,
   setEditMessageId,
 }: ChatMessagesProps) => {
+  // ref
+  const endMessageRef = useRef<HTMLDivElement | null>(null);
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+  // state
+  const [showScrollBtn, setShowScrollBtn] = useState(false);
+
   // hooks
   const pathname = usePathname();
 
+  // function
+  const scrollToBottom = () => {
+    endMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    if (!container) return;
+
+    const handleScroll = () => {
+      const { scrollTop, scrollHeight, clientHeight } = container;
+      const scrollBottom = scrollHeight - scrollTop - clientHeight;
+      setShowScrollBtn(scrollBottom > 150);
+    };
+
+    container.addEventListener('scroll', handleScroll);
+    return () => container.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  // style
   const menuHeight =
     pathname.includes('admin') && !showMenu
       ? 'max-h-[calc(100vh-200px)] lg:max-h-[calc(100vh-160px)] '
@@ -25,19 +59,37 @@ const ChatMessages = ({
           : 'max-h-[calc(100vh-380px)]';
 
   return (
-    <div
-      className={`h-full w-full overflow-y-auto rounded-t-md p-2 lg:p-4 ${menuHeight} `}
-    >
-      {messages.map((message) => (
-        <ChatMessageItem
-          key={message.id}
-          message={message}
-          handleTemplateSelect={() => {
-            handleTemplateSelect?.(message.text as string);
-            setEditMessageId?.(message.id as string);
-          }}
+    <div className="relative h-full w-full">
+      <div
+        className={`h-full w-full overflow-y-auto rounded-t-md p-2 lg:p-4 ${menuHeight} `}
+        ref={scrollContainerRef}
+      >
+        {messages.map((message) => (
+          <ChatMessageItem
+            key={message.id}
+            message={message}
+            handleTemplateSelect={() => {
+              handleTemplateSelect?.(message.text as string);
+              setEditMessageId?.(message.id as string);
+            }}
+          />
+        ))}
+
+        <div ref={endMessageRef} />
+      </div>
+
+      {showScrollBtn && (
+        <ButtonFreeClass
+          onClick={scrollToBottom}
+          className="z-9 absolute right-1/2 top-[32px]"
+          icon={
+            <MyIcon
+              icon="uiw:down-circle"
+              className="text-h3 text-primary-500/50 hover:text-primary-700"
+            />
+          }
         />
-      ))}
+      )}
     </div>
   );
 };
--- src/components/organisms/sidebar-component/chat-sidebar/index.tsx
+++ src/components/organisms/sidebar-component/chat-sidebar/index.tsx
@@ -2,10 +2,13 @@
 
 import { useSelector } from 'react-redux';
 import { RootState } from '@/src/store';
+import { useEffect, useRef, useState } from 'react';
 
 // ui
 import AdminSupportUserCard from '@/src/components/molecule/cards/admin-support-user-card';
 import PageLoading from '@/src/components/common/page-loading';
+import ButtonFreeClass from '@/src/components/atom/buttons-component/button-free-class';
+import MyIcon from '@/src/components/atom/icon-components';
 
 // type
 import { chatSidebarProps } from '../../type';
@@ -16,12 +19,34 @@ import { useChatsListener } from '@/src/hooks/chat/use-user-listener';
 const ChatSidebar = ({
   setShowSidebar,
 }: Pick<chatSidebarProps, 'setShowSidebar'>) => {
+  // ref
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+  // state
+  const [showScrollBtn, setShowScrollBtn] = useState(false);
   // redux
   const chats = useSelector((state: RootState) => state.chats.chats);
   const chatsFilters = useSelector((state: RootState) => state.chatsFilters);
 
-  // functions
+  // hook
   useChatsListener();
+
+  // function
+  const scrollToTop = () => {
+    scrollContainerRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    if (!container) return;
+
+    const handleScroll = () => {
+      const { scrollTop } = container;
+      setShowScrollBtn(scrollTop > 200);
+    };
+
+    container.addEventListener('scroll', handleScroll);
+    return () => container.removeEventListener('scroll', handleScroll);
+  }, []);
 
   // apply filters
   const finalChats = chats.filter((chat) => {
@@ -41,12 +66,27 @@ const ChatSidebar = ({
   if (!finalChats) return <PageLoading />;
 
   return (
-    <div className="h-full w-full">
-      {finalChats.map((chat) => (
-        <div key={chat.id} className="mb-4">
-          <AdminSupportUserCard chat={chat} setShowSidebar={setShowSidebar} />
-        </div>
-      ))}
+    <div className="relative h-full w-full">
+      <div className="h-full w-full">
+        {finalChats.map((chat) => (
+          <div key={chat.id} className="mb-4">
+            <AdminSupportUserCard chat={chat} setShowSidebar={setShowSidebar} />
+          </div>
+        ))}
+      </div>
+
+      {showScrollBtn && (
+        <ButtonFreeClass
+          onClick={scrollToTop}
+          className="z-9 absolute right-0 top-0"
+          icon={
+            <MyIcon
+              icon="uiw:down-circle"
+              className="rotate-180 text-h3 text-primary-500/50 hover:text-primary-700"
+            />
+          }
+        ></ButtonFreeClass>
+      )}
     </div>
   );
 };
