fb53376404f50d90b7282c7e8d6dceca441ddbc7
omidKianfar <kianfar0991@gmail.com>
2026-05-30 15:47:23 +0330

    fix jest errors

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/$1',
   },
 };
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,10 @@
-import "@testing-library/jest-dom";
-import "whatwg-fetch";
+import '@testing-library/jest-dom';
+import 'whatwg-fetch';
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (args[0] && args[0].includes('ReactDOMTestUtils.act')) {
+    return;
+  }
+  originalError(...args);
+};
