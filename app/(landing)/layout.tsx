697c52e437572c08b4eda71160691023e93c1aa0
omidKianfar <kianfar0991@gmail.com>
2026-06-01 00:45:01 +0330

    fix about page

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { PropsWithChildren, useState } from 'react';
+import Header from '@/src/components/organisms/header-component/indx';
+import SideBar from '@/src/components/organisms/sidebar-component/user-sidebar';
+
+const DashboardLayout = ({ children }: PropsWithChildren) => {
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
+
+  return (
+    <>
+      <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+
+      <SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+
+      <div onClick={() => setShowSidebar(false)}>{children}</div>
+    </>
+  );
+};
+
+export default DashboardLayout;
