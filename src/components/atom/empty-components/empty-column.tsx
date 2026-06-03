43c593e0752c91cf1870ce77a8de360b5aa28220
omidKianfar <kianfar0991@gmail.com>
2026-06-03 17:21:05 +0330

    fix empty component styles

--- src/components/atom/empty-components/empty-column.tsx
+++ src/components/atom/empty-components/empty-column.tsx
@@ -1,6 +1,6 @@
 const EmptyColumn = () => {
   return (
-    <div className="flex h-full items-center justify-center rounded-lg border border-gray-300 p-2 text-center text-body shadow-md">
+    <div className="flex h-full items-center justify-center p-2 text-center text-body">
       <p className="text-gray-400">No Data</p>
     </div>
   );
