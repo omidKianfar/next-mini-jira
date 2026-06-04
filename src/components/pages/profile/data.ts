c7ebac2c4f407d47e36bc202426743dcf9b25471
omidKianfar <kianfar0991@gmail.com>
2026-06-04 12:54:54 +0330

    fix bugs

--- src/components/pages/profile/data.ts
+++ src/components/pages/profile/data.ts
@@ -1,4 +1,4 @@
 export const backModalMessage = {
   title: 'Your account has been created.',
-  description: `If you return to the signup step, you will no longer be able to register with this account.<br />You will need to log in to the app and complete the payment and profile setup separately.`,
+  description: `If you return to the signup step, you will no longer be able to register with this account. You will need to log in to the app and complete the payment and profile setup separately.`,
 };
